aa91e8c07b89a4d52e535c2ba34d44bc83aee0b4
talkasrul <talkasrul@gmail.com>
2018-09-07 20:22:44 +0700

    Add Task Route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const task = require('./taskRoute')
+
+router.use('/tasks', task)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Halo Asrul H');
-});
+  res.send('Halo Asrul H')
+})
 
-module.exports = router;
+module.exports = router
---
+++ server/routes/taskRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const isLogin = require('../midlewares/isLogin')
+const { createOne, getAllDo, getAllDone, getOne, setDo, setDone, softdelete, unsoftdelete, updateOne, deleteOne } = require('../controllers/taskController')
+
+router.post('/', isLogin, createOne)
+router.get('/', isLogin, getAllDo)
+router.get('/done', isLogin, getAllDone)
+router.get('/:id', isLogin, getOne)
+router.patch('/:id/do', isLogin, setDo)
+router.patch('/:id/done', isLogin, setDone)
+router.patch('/:id/delete', isLogin, softdelete)
+router.patch('/:id/restore', isLogin, unsoftdelete)
+router.update('/:id', isLogin, updateOne)
+router.delete('/:id', isLogin, deleteOne)
+
+module.exports = router;
